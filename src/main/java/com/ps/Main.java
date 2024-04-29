0d9ad942ca20a80670dc423f8e2a59c3f66cb566
Jeremy <ogprago@gmail.com>
2024-04-29 09:59:22 -0400

    Created Home Screen Display

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,35 @@
 package com.ps;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String userIn;
+        do {
+            System.out.println("Welcome. Please select an option: ");
+            System.out.println("\tD) Add Deposit");
+            System.out.println("\tP) Make Payment (Debit)");
+            System.out.println("\tL) Ledger");
+            System.out.println("\tX) Exit");
+            userIn = scanner.nextLine().toUpperCase();
+
+            switch (userIn) {
+                case "D":
+                    break;
+                case "P":
+                    break;
+                case "L":
+                    break;
+                case "X":
+                    System.out.println("Exiting....");
+                    break;
+                default:
+                    System.out.println("Command not found");
+
+            }
+
+        } while (!userIn.equalsIgnoreCase("X"));
     }
 }
